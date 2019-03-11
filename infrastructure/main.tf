263d3e99d6955ad3af131e96a081b274a82459aa
Chris Moreton <christopher.moreton@hmcts.net>
2019-03-11 11:16:14 +0000

    Update Terraform config for pipeline

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,27 @@
 provider "azurerm" {}
 
 locals {
-  ase_name               = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  ase_name       = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
+  azureVaultName = "sscs-${var.env}"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.product}-${var.component}-${var.env}"
+  location = "${var.location_app}"
+
+  tags = "${merge(var.common_tags,
+    map("lastUpdated", "${timestamp()}")
+    )}"
+}
+
+data "azurerm_key_vault" "sscs_key_vault" {
+  name                = "${local.azureVaultName}"
+  resource_group_name = "${local.azureVaultName}"
+}
+
+data "azurerm_key_vault_secret" "idam_api" {
+  name      = "idam-api"
+  vault_uri = "${data.azurerm_key_vault.sscs_key_vault.vault_uri}"
 }
 
 module "sscs-ccd-callback-orchestrator" {
@@ -18,5 +38,8 @@ module "sscs-ccd-callback-orchestrator" {
   app_settings = {
     LOGBACK_REQUIRE_ALERT_LEVEL = "false"
     LOGBACK_REQUIRE_ERROR_CODE  = "false"
+
+    IDAM_API_URL = "${data.azurerm_key_vault_secret.idam_api.value}"
+
   }
 }
