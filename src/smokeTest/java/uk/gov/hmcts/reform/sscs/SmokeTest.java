e1f3abf7b242f6ba821286477371ac36c3832ab6
Chris Moreton <christopher.moreton@hmcts.net>
2019-03-18 20:42:17 +0000

    Fix dummy smoke test

--- src/smokeTest/java/uk/gov/hmcts/reform/sscs/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/sscs/SmokeTest.java
@@ -1,18 +1,18 @@
-package uk.gov.hmcts.reform.sscs.smoke;
-
-import static org.junit.Assert.assertEquals;
+package uk.gov.hmcts.reform.sscs;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class SmokeTest {
 
     @Test
-    public void smokeTest() {
+    public void assertOneEqualsOne() {
 
         assertEquals(1,1);
     }
