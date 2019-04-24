07467aed6fbfd9d289912e5121f090195935946b
Ryan Da Costa <ryan.dacosta@hmcts.net>
2019-04-24 15:31:00 +0100

    move dots from environment variables.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -72,7 +72,7 @@ module "sscs-ccd-callback-orchestrator" {
 
     IDAM_API_URL = "${data.azurerm_key_vault_secret.idam_api.value}"
 
-    IDAM.S2S-AUTH.TOTP_SECRET  = "${data.azurerm_key_vault_secret.sscs_s2s_secret.value}"
+    IDAM_S2S_AUTH_TOTP_SECRET  = "${data.azurerm_key_vault_secret.sscs_s2s_secret.value}"
     IDAM_S2S_AUTH              = "${local.s2sCnpUrl}"
     IDAM_S2S_AUTH_MICROSERVICE = "${var.ccd_idam_s2s_auth_microservice}"
 
