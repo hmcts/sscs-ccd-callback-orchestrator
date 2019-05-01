dc48a09516a7e4cce8e344f19db10440e2ec37ab
Ryan Da Costa <369407+ryandac@users.noreply.github.com>
2019-05-01 09:34:07 +0100

    Fix CCD Callback so that an error message does not trigger on a CCD event. (#41)

--- src/test/java/uk/gov/hmcts/reform/sscs/controllers/CcdCallbackOrchestratorControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/sscs/controllers/CcdCallbackOrchestratorControllerTest.java
@@ -3,9 +3,11 @@ package uk.gov.hmcts.reform.sscs.controllers;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.springframework.http.ResponseEntity;
 import uk.gov.hmcts.reform.sscs.service.AuthorisationService;
 import uk.gov.hmcts.reform.sscs.servicebus.TopicPublisher;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -30,7 +32,9 @@ public class CcdCallbackOrchestratorControllerTest {
 
     @Test
     public void shouldCreateAndSendNotificationForSscsCaseData() {
-        controller.send("", MESSAGE);
+        ResponseEntity<String> responseEntity = controller.send("", MESSAGE);
         verify(topicPublisher).sendMessage(eq(MESSAGE));
+        assertEquals(200, responseEntity.getStatusCode().value());
+        assertEquals("{}", responseEntity.getBody());
     }
 }
