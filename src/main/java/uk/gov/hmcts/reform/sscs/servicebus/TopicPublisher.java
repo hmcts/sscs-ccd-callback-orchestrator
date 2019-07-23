8a5fb971c399e9ac92edfd984ddc773ad7c5b0fd
srochani <sunil.rochani@hotmail.com>
2019-07-23 09:53:24 +0100

    SSCS-6138 Evidence share – Exception from the queue

--- src/main/java/uk/gov/hmcts/reform/sscs/servicebus/TopicPublisher.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/servicebus/TopicPublisher.java
@@ -3,12 +3,15 @@ package uk.gov.hmcts.reform.sscs.servicebus;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.IllegalStateException;
+import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
+import javax.jms.ConnectionFactory;
 import javax.jms.Session;
 
 @Service
@@ -18,6 +21,9 @@ public class TopicPublisher {
     private final JmsTemplate jmsTemplate;
 
     private final String destination;
+
+    @Autowired
+    private ConnectionFactory connectionFactory;
 
     @Autowired
     public TopicPublisher(JmsTemplate jmsTemplate, @Value("${amqp.topic}") final String destination) {
@@ -31,8 +37,20 @@ public class TopicPublisher {
     )
     public void sendMessage(final String message) {
         log.info("Sending message.");
-        jmsTemplate.send(destination, (Session session) -> session.createTextMessage(message));
-        log.info("Message sent.");
+        try {
+            jmsTemplate.send(destination, (Session session) -> session.createTextMessage(message));
+            log.info("Message sent.");
+        } catch (IllegalStateException e) {
+            if (connectionFactory instanceof CachingConnectionFactory) {
+                log.info("Send failed, attempting to reset connection...");
+                ((CachingConnectionFactory) connectionFactory).resetConnection();
+                log.info("Resending..");
+                jmsTemplate.send(destination, (Session session) -> session.createTextMessage(message));
+                log.info("In catch, message sent.");
+            } else {
+                throw e;
+            }
+        }
     }
 
     @Recover
