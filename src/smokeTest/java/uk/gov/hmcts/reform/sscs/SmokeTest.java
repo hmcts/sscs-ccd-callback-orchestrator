118c0c9b01c74736e97600a70ee73e8c6dff62fb
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2020-07-31 11:50:12 +0100

    fix integration tests

--- src/smokeTest/java/uk/gov/hmcts/reform/sscs/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/sscs/SmokeTest.java
@@ -5,10 +5,13 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.reform.idam.client.IdamApi;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@EnableFeignClients(basePackageClasses = {IdamApi.class})
 public class SmokeTest {
 
     @Test
