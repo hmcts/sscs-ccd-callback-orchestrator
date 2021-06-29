a6f28fa574529eee78142b42bad098752b16f478
gokul-sol <gokul.sridharan@HMCTS.NET>
2021-06-29 13:42:26 +0100

    copy cmc security script

--- security.sh
+++ security.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 
+# echo ${TEST_URL}
+# zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1005 -l FAIL
+# cat zap.out
+# zap-cli --zap-url http://0.0.0.0 -p 1005 report -o /zap/api-report.html -f html
+# cp /zap/api-report.html functional-output/
+# curl --fail http://0.0.0.0:1005/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
+# cp *.* functional-output/
+# zap-cli --zap-url http://0.0.0.0 -p 1005 alerts -l Medium --exit-code False
+
+#!/usr/bin/env bash
 echo ${TEST_URL}
-zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1005 -l FAIL
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -S -d -u ${SecurityRules} -P 1001 -l FAIL
 cat zap.out
-zap-cli --zap-url http://0.0.0.0 -p 1005 report -o /zap/api-report.html -f html
+echo "ZAP has successfully started"
+zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
+mkdir -p functional-output
+chmod a+wx functional-output
+curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 cp /zap/api-report.html functional-output/
-curl --fail http://0.0.0.0:1005/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
-cp *.* functional-output/
-zap-cli --zap-url http://0.0.0.0 -p 1005 alerts -l Medium --exit-code False
+zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Medium --exit-code False
+
