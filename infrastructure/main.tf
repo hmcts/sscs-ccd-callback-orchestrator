c56cd127d427f9ae382ee46a83e151cb7cd74283
Jarek Pierchala <jaroslaw.pierchala@hmcts.net>
2022-11-29 14:47:00 +0000

    added terraform tags for resource

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,3 +10,14 @@ data "azurerm_key_vault" "sscs_key_vault" {
   name                = local.azureVaultName
   resource_group_name = local.azureVaultName
 }
+
+# variables.tf common_tags are being injected by jenkins pipeline
+locals {
+  tags = (merge(
+    var.common_tags,
+    tomap({
+      "Team Contact" = "#sscs"
+      "Team Name"    = "SSCS Team"
+    })
+  ))
+}
