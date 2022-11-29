753d40d620406e158ae47d14f53f482d6eab8a94
Jarek Pierchala <jaroslaw.pierchala@hmcts.net>
2022-11-29 15:18:26 +0000

    removed redundant locals block

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,15 +4,6 @@ provider "azurerm" {
 
 locals {
   azureVaultName = "sscs-${var.env}"
-}
-
-data "azurerm_key_vault" "sscs_key_vault" {
-  name                = local.azureVaultName
-  resource_group_name = local.azureVaultName
-}
-
-# variables.tf common_tags are being injected by jenkins pipeline
-locals {
   tags = (merge(
     var.common_tags,
     tomap({
@@ -20,4 +11,9 @@ locals {
       "Team Name"    = "SSCS Team"
     })
   ))
+}
+
+data "azurerm_key_vault" "sscs_key_vault" {
+  name                = local.azureVaultName
+  resource_group_name = local.azureVaultName
 }
