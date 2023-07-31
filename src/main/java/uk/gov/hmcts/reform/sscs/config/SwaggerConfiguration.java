cb8a3b02ce7860c691936cbf36833cbe42ab220c
hajam09 <hajam09@yahoo.com>
2023-07-31 17:01:44 +0100

    revet - java 17

--- src/main/java/uk/gov/hmcts/reform/sscs/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/SwaggerConfiguration.java
@@ -1,21 +1,26 @@
 package uk.gov.hmcts.reform.sscs.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import uk.gov.hmcts.reform.sscs.Application;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfiguration {
 
     @Bean
-    public OpenAPI ccdCallbackOrchestrator() {
-        return new OpenAPI()
-            .info(new Info().title("CCD Callback Orchestrator")
-                .description("CCD Callback Orchestrator")
-                .version("v0.0.1")
-                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .useDefaultResponseMessages(false)
+            .select()
+            .apis(RequestHandlerSelectors.basePackage(Application.class.getPackage().getName() + ".controllers"))
+            .paths(PathSelectors.any())
+            .build();
     }
 
 }
