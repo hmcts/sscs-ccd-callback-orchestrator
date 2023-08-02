aa5633ff9e2ad67e40f31e551f406dfa29084ae0
Andrei Trushkov <andrei.trushkov@hmcts.net>
2023-08-02 15:09:59 +0100

    j17 test

--- src/main/java/uk/gov/hmcts/reform/sscs/config/ServiceTokenGeneratorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/sscs/config/ServiceTokenGeneratorConfiguration.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.sscs.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,7 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
 
 @Configuration
 @Lazy
+@Slf4j
 public class ServiceTokenGeneratorConfiguration {
 
     @Bean
@@ -18,6 +20,7 @@ public class ServiceTokenGeneratorConfiguration {
         @Value("${idam.s2s-auth.microservice}") final String microService,
         final ServiceAuthorisationApi serviceAuthorisationApi
     ) {
+        log.info("####### Test {}", microService);
         return AuthTokenGeneratorFactory.createDefaultGenerator(secret, microService, serviceAuthorisationApi);
     }
 
